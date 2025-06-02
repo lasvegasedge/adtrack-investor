0013c85d1eea5940a65a89ba1e1973a3de117b88
lasvegasedge <josephsonlineworld@gmail.com>
2025-06-02 10:11:47 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
-import os
-import sys
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))  # DON'T CHANGE THIS !!!
-
 from src import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
