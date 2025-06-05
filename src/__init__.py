53eeabb551018d3b148d39369ebc0ab1d6d5f66b
lasvegasedge <josephsonlineworld@gmail.com>
2025-06-05 02:07:47 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,10 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'temporary-secret-key'
-    
     @app.route('/')
-    def index():
-        return "AdTrack Investor Presentation Website - Coming Soon!"
-    
+    def home():
+        return 'Hello from AdTrack Investor!'
     return app
