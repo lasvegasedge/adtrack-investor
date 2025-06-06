d3850a62f37c4fcf8470a76d5ae152c75103d8b8
lasvegasedge <josephsonlineworld@gmail.com>
2025-06-05 18:20:27 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
+    load_dotenv()
+
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'temporary-secret-key'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'temporary-secret-key')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    login_manager.init_app(app)
 
     from .routes import routes as routes_blueprint
     app.register_blueprint(routes_blueprint)
