e362eea919ddb0cd752df82547490398c2fb2e46
lasvegasedge <josephsonlineworld@gmail.com>
2025-06-05 17:26:39 -0700

    Update __init__.py

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 routes = Blueprint('routes', __name__)
 
 @routes.route('/')
 def index():
-    return "AdTrack Investor — Now Live!"
+    return render_template('index.html')
