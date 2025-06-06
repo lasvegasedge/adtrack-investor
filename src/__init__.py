ba8f0f31a0cd0cbaa23d70873f85205b36ad05b8
lasvegasedge <josephsonlineworld@gmail.com>
2025-06-05 17:11:42 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    @app.route('/')
-    def home():
-        return 'Hello from AdTrack Investor!'
+    app.config['SECRET_KEY'] = 'temporary-secret-key'
+
+    from .routes import routes as routes_blueprint
+    app.register_blueprint(routes_blueprint)
+
     return app
